8ee32878d07bc8e8e566eef5d6bfa1fb3fda6616
Tiffany Chen <tchen@thoughtworks.com>
2016-12-29 12:25:31 -0500

    Display name and add two-way data binding.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+          <input [(ngModel)]="hero.name" placeholder="name">
+      `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+    title = 'Tour of Heroes';
+    hero = Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
